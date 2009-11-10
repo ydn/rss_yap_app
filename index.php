0f6e834e623ff2b1007ed21d44ad6af92f80ee47
erik <erikeldridge@yahoo.com>
2009-11-09 16:40:39 -0800

    reformatted code.  adjusted margins and padding.  standardized font size

--- index.php
+++ index.php
@@ -1,21 +1,5 @@
 <?php 
 
-$title = 'app title';
-$url = 'http://rss.news.yahoo.com/rss/topstories';
-$siteImage = 'http://yap-studio.com/apps/rss/assets/NoPreview.png';
-
-
-if(!isset($siteImage))
-{
-
-$siteImage = "http://yap-studio.com/apps/rss/assets/NoPreview.png";
-}
-else
-{
-
-}
-
-
 $insideitem = false;
 $tag = "";
 $title = "";
@@ -34,35 +18,34 @@ $insideitem = true;
 }
 
 function endElement($parser, $name) {
-global $insideitem, $tag, $title, $description, $link;
-$article = "<div class='article'>";
-if ($name == "ITEM") {
-print($article);
-printf("<div class='title'><a href='%s'>%s</a></div>", trim($link), htmlspecialchars(trim($title)) );
-//printf('<div class="description">%s</div></div>',htmlspecialchars(trim($description)));
-printf('<div class="description">%s</div></div>',$description);
-$title = "";
-$description = "";
-$link = "";
-$insideitem = false;
-}
+    global $insideitem, $tag, $title, $description, $link;
+    $article = "<div class='article'>";
+    if ($name == "ITEM") {
+        print($article);
+        printf("<div class='title'><a href='%s'>%s</a></div>", trim($link), htmlspecialchars(trim($title)) );
+        printf('<div class="description">%s</div></div>',$description);
+        $title = "";
+        $description = "";
+        $link = "";
+        $insideitem = false;
+    }
 }
 
 function characterData($parser, $data) {
-global $insideitem, $tag, $title, $description, $link;
-if ($insideitem) {
-switch ($tag) {
-case "TITLE":
-$title .= $data;
-break;
-case "DESCRIPTION":
-$description .= $data;
-break;
-case "LINK":
-$link .= $data;
-break;
-}
-}
+    global $insideitem, $tag, $title, $description, $link;
+    if ($insideitem) {
+        switch ($tag) {
+            case "TITLE":
+                $title .= $data;
+                break;
+            case "DESCRIPTION":
+                $description .= $data;
+                break;
+            case "LINK":
+                $link .= $data;
+            break;
+        }
+    }
 }
 
 function loadFeed($url)
@@ -98,87 +81,71 @@ color: #660066;
 
 
 #header {
-position:relative;
-background-image:url('http://yap-studio.com/apps/rss/assets/gsprite_mod_default_103008.gif');
-background-repeat:repeat-x;
-background-position:0 -580px;
-height: 100px;
-
+    font-size: 12px;
+    padding: 30px 0;
+    margin-bottom: 30px;
+    position:relative;
+    background-image:url('http://yap-studio.com/apps/rss/assets/gsprite_mod_default_103008.gif');
+    background-repeat:repeat-x;
+    background-position:0 -580px;
+    height: 100px;
+    border-bottom: 1px solid #ccc;
 }
+
 #header .colImage {
-width:105px;
-color:blue;
-padding:5px;
-margin:3px 3px 5px 3px;
-float:left;
+    width:105px;
+    float:left;
+    margin-right: 30px;
 }
 
 
 #header .col {
-width:250px;
-color:blue;
-padding:5px;
-margin:3px 3px 5px 3px;
-float:left;
+    color:blue;
 }
 
 #wrapper {
-/*width:395px;*/
-position:relative;
+    font-size: 12px;
 }
 
 .headOne {
-font-size: 14px;
-color: #365B8A;
-font-weight: bold;
-width: 260px;	
-padding:5px;
-margin:3px 3px 10px 3px;
+    font-size: 150%;
+    color: #365B8A;
+    font-weight: bold;
+    width: 260px;	
+    padding:5px;
+    margin:3px 3px 10px 3px;
 }
 
 .description {
-font-size: 13px;
-color: #222222;
+    font-size: 125%;
+    color: #222222;
 }
 
 .grayUrl {
-font-size: 10px;
-color: #888888;
-width: 260px;
-}
-
-.headTwo {
-font-size: 13px;
-color: #3399FF;
-font-weight: bold;
-text-transform: uppercase;
+    font-size: 75%;
+    color: #888888;
+    width: 260px;
 }
 
 span.bottom {
-position:absolute;
-left:3px;
-bottom:3px;
-padding:5px;
+    position:absolute;
+    left:3px;
+    bottom:3px;
+    padding:5px;
 }
 
 .article {
-font-family: Verdana, Arial, Helvetica, sans-serif;
-color:#FFFFFF;
-margin-bottom:20px;
+    font-family: Verdana, Arial, Helvetica, sans-serif;
+    color:#FFFFFF;
+    margin-bottom:20px;
 }
+
 .article .title {
-font-size: 12px;
-font-weight: bold;
-margin-left:15px;
+    font-weight: bold;
 }
-
-
-
 
 .article .description {
-font-size: 12px;
-margin-left:15px;
-margin-right:15px;
+margin-right:30px;
 }
 
 .footer {
@@ -189,35 +156,34 @@ margin-right:15px;
 }
 </style>
 
+<?php
+$title = 'app title';
+$url = 'http://rss.news.yahoo.com/rss/topstories';
+$siteImage = 'http://yap-studio.com/apps/rss/assets/NoPreview.png';
+?>
 
 <div id="header">
 	<div class="colImage"><img src="<?php echo $siteImage; ?>" width="102" height="102"></div>
-
-	<div class="col">
-		<div align="left" class="headOne"><a href="<?php echo $url; ?>"><?php echo $title; ?></a></div>
-		<div align="left" class="description">Website Description would go here</div>
-		<div align="left" class="grayUrl"> more >></div>
+    	<div class="col">
+    		<div align="left" class="headOne"><a href="<?php echo $url; ?>"><?php echo $title; ?></a></div>
+    		<div align="left" class="description">Website Description would go here</div>
+    		<div align="left" class="grayUrl"> more >></div>
+    	</div>
+	    <div style="clear:both;">
 	</div>
-	<div>
-
-
-	<div style="clear:both;">
-	</div>
-
 </div>
-	<hr>
+
 <div id="wrapper"> 
-	<div align="left" class="headOne"><?php echo $title; ?></div>
-<?php
+    <?php
 
-$data = loadFeed($url);
+    $data = loadFeed($url);
 
-$xml_parser = xml_parser_create();
-xml_set_element_handler($xml_parser, "startElement", "endElement");
-xml_set_character_data_handler($xml_parser, "characterData");
-xml_parse($xml_parser, $data) or die(sprintf("XML error: %s at line %d", xml_error_string(xml_get_error_code($xml_parser)), xml_get_current_line_number($xml_parser)));
-xml_parser_free($xml_parser);
-?>
+    $xml_parser = xml_parser_create();
+    xml_set_element_handler($xml_parser, "startElement", "endElement");
+    xml_set_character_data_handler($xml_parser, "characterData");
+    xml_parse($xml_parser, $data) or die(sprintf("XML error: %s at line %d", xml_error_string(xml_get_error_code($xml_parser)), xml_get_current_line_number($xml_parser)));
+    xml_parser_free($xml_parser);
+    ?>
 </div>
 
 <div class="footer">
